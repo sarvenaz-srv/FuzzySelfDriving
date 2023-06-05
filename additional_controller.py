1828544e9498694dadbe13733d0ea8dac5b4b42f
sarvenaz-srv <sarvghad79@gmail.com>
2023-06-05 19:25:02 +0330

    initial commit

---
+++ additional_controller.py
@@ -0,0 +1,16 @@
+class FuzzyGasController:
+    """
+    # emtiazi todo
+    write all the fuzzify,inference,defuzzify method in this class
+    """
+
+    def __init__(self):
+        pass
+        
+
+    def decide(self, center_dist):
+        """
+        main method for doin all the phases and returning the final answer for gas
+        """
+        return 30
+    
---
+++ fuzzy_controller.py
@@ -0,0 +1,16 @@
+class FuzzyController:
+    """
+    #todo
+    write all the fuzzify,inference,defuzzify method in this class
+    """
+
+    def __init__(self):
+        pass
+
+
+    def decide(self, left_dist,right_dist):
+        """
+        main method for doin all the phases and returning the final answer for rotation
+        """
+        return 0
+    
