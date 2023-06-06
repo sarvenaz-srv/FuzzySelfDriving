78d8cc24dd6a810c0e2350f847c5ab274c7dacbe
sarvenaz-srv <sarvghad79@gmail.com>
2023-06-06 15:21:03 +0330

    implement inputs fuzzification

--- fuzzy_controller.py
+++ fuzzy_controller.py
@@ -12,5 +12,34 @@ class FuzzyController:
         """
         main method for doin all the phases and returning the final answer for rotation
         """
+        left_membership = self.fuzzification(left_dist) # [left_close, left_moderate, left_far]
+        right_membership = self.fuzzification(right_dist) # [right_close, right_moderate, right_far]
+        
+        
         return 0
+    
+    def fuzzification(self, dist):
+        # gets right or left distance and returns its fuzzy membership as a list
+        # returns [close_membership, moderate_membership, far_membership]
+        membership = []
+        
+        close_membership = 0
+        if(dist >= 0 and dist <=50):
+            close_membership = -dist/50 + 1
+        membership.append(close_membership)
+        
+        moderate_membership = 0
+        if(dist >= 35 and dist < 50):
+            moderate_membership = (dist - 35)/15
+        elif(dist >= 50 and dist <= 65):
+            moderate_membership = (-dist + 65)/15
+        membership.append(moderate_membership)
+        
+        far_membership = 0
+        if(dist >= 50 and dist <= 100):
+            far_membership = dist/50 - 1
+        membership.append(far_membership)
+        
+        return membership
+
     
